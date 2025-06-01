1743dc82d2e9d1b31528626944166d6cb1414eff
Asif Ahmed <asifahmed9510@gmail.com>
2025-06-01 18:56:42 +0600

    v2

---
+++ Alibaba_v2/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Alibaba_Game-1.0";
+const contentToCache = [
+    "Build/Alibaba_v2.loader.js",
+    "Build/Alibaba_v2.framework.js.unityweb",
+    "Build/Alibaba_v2.data.unityweb",
+    "Build/Alibaba_v2.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
