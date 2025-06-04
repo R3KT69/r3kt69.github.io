bdcd4d1b18bba20e4aabd576b7865b2fad2462dd
Asif Ahmed <asifahmed9510@gmail.com>
2025-06-05 01:36:08 +0600

    new

---
+++ teenpatti_v4_final/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-teenpatti_shuvo-1.0";
+const contentToCache = [
+    "Build/teenpatti_v3_final.loader.js",
+    "Build/teenpatti_v3_final.framework.js.unityweb",
+    "Build/teenpatti_v3_final.data.unityweb",
+    "Build/teenpatti_v3_final.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
